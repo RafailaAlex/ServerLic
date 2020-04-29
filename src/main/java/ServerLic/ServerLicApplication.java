b8c4fa97236b12646f89c9ac389c96761cb28781
RafailaAlex <40004281+RafailaAlex@users.noreply.github.com>
2020-04-29 14:21:44 +0300

    Add files via upload

---
+++ src/main/java/ServerLic/ServerLicApplication.java
@@ -0,0 +1,13 @@
+package ServerLic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerLicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerLicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ServerLic/ServerLicApplicationTests.java
@@ -0,0 +1,13 @@
+package ServerLic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerLicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
