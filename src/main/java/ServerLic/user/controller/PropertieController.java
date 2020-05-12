783ad09251cedfaebaebf97c84d2afde1482d47a
RafailaAlex <alexandru.rafaila@yahoo.com>
2020-05-12 15:31:02 +0300

    update PropertieController

--- src/main/java/ServerLic/user/controller/PropertieController.java
+++ src/main/java/ServerLic/user/controller/PropertieController.java
@@ -1,12 +1,11 @@
 package ServerLic.user.controller;
 
 import ServerLic.user.domain.Propertie;
+import ServerLic.user.domain.User;
 import ServerLic.user.repository.IPropertieRepository;
 import ServerLic.user.service.IPropertieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +20,15 @@ public class PropertieController {
         this.propertieService = propertieService;
     }
     @GetMapping("/all")
-    public List<Propertie> getAllProperte(){
+    public List<Propertie> getAllPropertie(){
         return propertieRepository.findAll();
+    }
+    @PostMapping("/savepropertie")
+    public Propertie savePropertie(@RequestBody Propertie propertie) {
+        return  propertieRepository.save(propertie);
+    }
+    @GetMapping("/get/{id}")
+    public Propertie getPropertie(@PathVariable("id") Long id) {
+        return propertieRepository.getOne(id);
     }
 }
